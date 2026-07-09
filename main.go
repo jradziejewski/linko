a5f86ae0eb58704e51535efeef896ac9434cd645
jradziejewski <192462@student.ue.wroc.pl>
2026-07-09 12:18:32 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"boot.dev/linko/internal/store"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+
+	httpPort := flag.Int("port", 8899, "port to listen on")
+	dataDir := flag.String("data", "./data", "directory to store data")
+	flag.Parse()
+
+	status := run(ctx, cancel, *httpPort, *dataDir)
+	cancel()
+	os.Exit(status)
+}
+
+func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
+	st, err := store.New(dataDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create store: %v\n", err)
+		return 1
+	}
+	s := newServer(*st, httpPort, cancel)
+	var serverErr error
+	go func() {
+		serverErr = s.start()
+	}()
+
+	<-ctx.Done()
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.shutdown(shutdownCtx); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to shutdown server: %v\n", err)
+		return 1
+	}
+	if serverErr != nil {
+		fmt.Fprintf(os.Stderr, "server error: %v\n", serverErr)
+		return 1
+	}
+	return 0
+}
